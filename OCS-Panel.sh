b2be8792abc9e117b24dba3c7816fd8d6c46c0f0
Jbbarrozo <jelsonbbarrozo@gmail.com>
2017-07-15 11:39:52 +0800

    Update OCS-Panel.sh

--- OCS-Panel.sh
+++ OCS-Panel.sh
@@ -31,7 +31,7 @@ service nginx restart
 apt-get -y install git
 cd /home/vps/public_html
 git init
-git remote add origin https://github.com/jhelson15/OCSPanel.git
+git remote add origin https://github.com/SSHAnakSolo/OCSPanel.git
 #git remote add origin https://github.com/leedzung-autoscrip/leeocs.git
 git pull origin master
 #wget https://raw.githubusercontent.com/asyrafazhan/python/master/conf/OCS-Panel.tar
