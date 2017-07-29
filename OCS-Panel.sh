066eb4f4c81f135d09f7012d7dcdbd78e599e1bf
Jelson barrozo <jelsonbbarrozo@gmail.com>
2017-07-29 23:25:32 +0800

    Update OCS-Panel.sh

--- OCS-Panel.sh
+++ OCS-Panel.sh
@@ -16,7 +16,7 @@ rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
 mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old
 curl https://raw.githubusercontent.com/anggasa/worm/master/nginx.conf > /etc/nginx/nginx.conf
-curl https://raw.githubusercontent.com/anggasa/worm/master/vps.conf > /etc/nginx/conf.d/vps.conf
+curl https://raw.githubusercontent.com/jhelson15/re-construction/master/conf/vps.conf > /etc/nginx/conf.d/vps.conf
 sed -i 's/cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php5/fpm/php.ini
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 useradd -m vps
