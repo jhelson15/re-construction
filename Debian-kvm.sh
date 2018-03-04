84b75ca4af7fabedcb10f74049bb8d5b96aba57c
Jels barrozo <jelsonbbarrozo@gmail.com>
2018-03-04 12:19:32 +0000

    Update Debian-kvm.sh

--- Debian-kvm.sh
+++ Debian-kvm.sh
@@ -161,11 +161,11 @@ service squid3 restart
  rm /root/webmin-current.deb
  service webmin restart
  service vnstat restart
-cd /root
-wget http://www.webmin.com/jcameron-key.asc
-apt-key add jcameron-key.asc
-echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
-echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" >> /etc/apt/sources.list
+#cd /root
+ wget http://www.webmin.com/jcameron-key.asc
+ apt-key add jcameron-key.asc
+ echo "deb http://download.webmin.com/download/repository sarge contrib" >> /etc/apt/sources.list
+ echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib" >> /etc/apt/sources.list
  apt-get update
  apt-get -y install webmin
  sed -i 's/ssl=1/ssl=0/g' /etc/webmin/miniserv.conf
